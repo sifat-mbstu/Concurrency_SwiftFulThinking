846981f6f44ff970271ebfad779f8261a25d3843
Sifatul Islam <sifatul.islam@tigerit.com>
2023-11-28 11:35:37 +0600

    init commit

---
+++ Concurrency_SwiftFull_Thinking/Concurrency_SwiftFull_Thinking/Concurrency_SwiftFull_ThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Concurrency_SwiftFull_ThinkingApp.swift
+//  Concurrency_SwiftFull_Thinking
+//
+//  Created by Sifatul Islam on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Concurrency_SwiftFull_ThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Concurrency_SwiftFull_Thinking/Concurrency_SwiftFull_Thinking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Concurrency_SwiftFull_Thinking
+//
+//  Created by Sifatul Islam on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
