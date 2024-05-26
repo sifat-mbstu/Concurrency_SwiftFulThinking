6f5be05f27178c9b945a0a4b03a3bb87388e8c09
Sifatul Islam <sifatul.islam@tigerit.com>
2024-05-26 10:42:39 +0600

    update storyboard

--- Concurrency_SwiftFull_Thinking/Concurrency_SwiftFull_Thinking/ViewControllers/iCodeHomeVC.swift
+++ Concurrency_SwiftFull_Thinking/Concurrency_SwiftFull_Thinking/ViewControllers/iCodeHomeVC.swift
@@ -12,14 +12,23 @@ class iCodeHomeVC: UIViewController {
     @IBOutlet weak var backButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let image = UIImage(systemName: "arrowshape.backward.circle")
-        var cfg = UIButton.Configuration.bordered()
-        cfg.image = image
-        backButton.configuration = cfg
-        backButton.addSymbolEffect(.pulse)    }
+        setBackButtonUI()
+    }
     
     @IBAction func back(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
+}
 
+extension iCodeHomeVC {
+    private func setBackButtonUI() {
+        let buttonImage = UIImage(systemName: "arrowshape.backward.circle.fill")
+        var config = UIButton.Configuration.borderless()
+        config.image = buttonImage
+        config.buttonSize = .large
+        config.imagePadding = .zero
+        config.contentInsets =  NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        backButton.configuration = config
+        backButton.addSymbolEffect(.pulse.byLayer)
+    }
 }
